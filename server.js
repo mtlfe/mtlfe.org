aecc9fa156e1342dc6490c1cee681ac16c1dfd2f
Francois Lafortune <flafortune@praizedmedia.com>
2011-10-11 19:33:17 -0300

    Edited server.js via GitHub

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var connect = require('connect');
 module.exports = connect.createServer(function (req, res) {
   console.log(req.method, req.url);
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World vII\n');
 })
 
 
